FIX: board advance calls neighborhood calculation

python/grid/grid.py
@@ -9,7 +9,7 @@ import random
 
 
 class Cell():
-    def __init__(self, val=0, ngb=0, rand=False):
+    def __init__(self, val=0, ngb=0, rand=True):
         if rand:
             self.val = random.randint(0,1)
         else:
@@ -86,9 +86,13 @@ class ConvolveSquare():
 class Board():
     def __init__(self, x:int, y:int, z:int, rand=False):
         self.mat = buildMatrix(x, y, z, rand)
+        self.ck = ConvolveSquare(self.mat)
+        self.analyze_board()
+
         self.mx = x
         self.my = y
         self.mz = z
+
         self.zeros = np.zeros_like(self.mat)
 
 
@@ -103,12 +107,17 @@ class Board():
                     func(self.mat[z][y][x], coord=(x,y,z))
 
 
+    def analyze_board(self, layers=None):
+        if layers is None:
+            count_neighbors(arr=self.mat[0], ck=self.ck)
+
 
     def advance(self, layers=None):
         def _advance_cell(cell, **kwargs):
             cell.advance()
 
         self.iterfunc(_advance_cell, layers)
+        self.analyze_board()
     
 
 