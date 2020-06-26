a6f39f246d2b5d8e308e278942b82230e8a1ca44
newnativeabq <vincent.a.brandon@gmail.com>
2020-06-25 21:38:03 -0600

    ADD: functioning header and start/stop implemented

--- python/render/render.py
+++ python/render/render.py
@@ -6,11 +6,12 @@ Render Engine
 import kivy
 from kivy.app import App 
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 
 
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-
+from kivy.properties import ObjectProperty
 
 from mechanics import (
     initializeGame,
@@ -60,18 +61,85 @@ class BoardGrid(GridLayout):
         self.draw_cell_color()
 
 
+def initializeBoard(cols):
+    board = BoardGrid(cols=cols)
+    board.draw_cell_color()
+    return board
+
 
 
 class GameApp(App):
     def __init__(self, **kwargs):
         super(GameApp, self).__init__(**kwargs)
-        self.boardsize = 25
+        self.title = 'Game of Life'
+        self.boardsize = 10
+        self.epoch = 0
+        self.board = initializeBoard(self.boardsize)
+        self.header = self._draw_header(
+            BoxLayout(orientation='horizontal'))
+        
+        # Clock settings
+        self.rate = 1
+        self.active = False
+
+    def _step(self, dt=None):
+        self.board.advance_game(dt)
+        self.epoch += 1
+        self._update_header()
+
+
+    def _draw_header(self, header=None):
+        header.size_hint = (1, 0.125)
+        
+        self.startBtn = Button(
+            text = "Start",
+            size_hint = (1, 1),
+            on_press = self._update_control_button
+        )
+
+        self.counter = Label(
+            text = f'Epoch {self.epoch}',
+            size_hint = (1, 1)
+        )
+
+        header.add_widget(self.startBtn)
+        header.add_widget(self.counter)
+        return header
+
+
+    def _update_header(self):
+        self.counter.text = f'Epoch {self.epoch}'
+
+
+    
+    def start(self):
+        self.startBtn.text = 'Stop'
+        self.active = True
+        Clock.unschedule(self._step)
+        Clock.schedule_interval(self._step, self.rate)
+
+    def stop(self):
+        self.startBtn.text = 'Start'
+        self.active = False
+        Clock.unschedule(self._step)
+
+    def _update_control_button(self, dt):
+        if self.active:
+            self.stop()
+        else:
+            self.start()
+
 
     def build(self):
-        board = BoardGrid(cols=self.boardsize)
-        board.draw_cell_color()
-        Clock.schedule_interval(board.advance_game, 60.0/60.0)
-        return board
+        gameWindow = BoxLayout(orientation='vertical')
+        
+        gameWindow.add_widget(self.header)
+        gameWindow.add_widget(self.board)
+
+        # Clock.schedule_interval(self._step, self.rate)
+        # self.start()
+
+        return gameWindow
 
 
 
